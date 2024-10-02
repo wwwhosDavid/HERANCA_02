9ed0e9ae921e92740696633f6ffd81462c17a1ef
wwwhosDavid <davidrodriguess.com.br22@gmail.com>
2024-10-01 23:12:29 -0300

    Add files via upload

---
+++ classePessoa.java
@@ -0,0 +1,42 @@
+// Criando a classe pessoa
+class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // Usando métodos get e set pois as variáveis estão encapsuladas
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + ", Cpf: " + cpf + ", Data de Nascimento: " + dataNascimento;
+    }
+}
